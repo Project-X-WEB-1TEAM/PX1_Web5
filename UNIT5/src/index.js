53c1a6789ab7a751eed4750ff06d5866a7ad4d27
parkjoungwan <whddhs6645@gmail.com>
2023-07-25 13:34:32 +0900

    [EDIT] add nginxconfg

--- UNIT5/src/index.js
+++ UNIT5/src/index.js
@@ -1,49 +1,26 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
+  var form = document.querySelector("form");
 
-  // // Toggle menu links and log to console when clicked
-  // var menuLinks = document.querySelectorAll("nav a, aside li a");
-  // menuLinks.forEach(function (link) {
-  //   link.addEventListener("click", function (e) {
-  //     e.preventDefault();
-  //     this.classList.toggle("active");
-  //     console.log(
-  //       this.textContent +
-  //         " is " +
-  //         (this.classList.contains("active") ? "active" : "inactive")
-  //     );
-  //   });
-  // });
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  // Handle form submission
-  document.querySelector("form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    var form = document.querySelector("form");
+    var bookName = document.getElementById("bookName");
+    var author = document.getElementById("author");
+    var publisher = document.getElementById("publisher");
 
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
+    var bookInfo = {
+      bookName: bookName.value,
+      author: author.value,
+      publisher: publisher.value,
+    };
 
-      var bookName = document.getElementById("bookName");
-      var author = document.getElementById("author");
-      var publisher = document.getElementById("publisher");
+    var bookInfoArray = JSON.parse(localStorage.getItem("bookInfo")) || [];
+    bookInfoArray.push(bookInfo);
+    alert("bookinfo inserted.");
 
-      var bookInfo = {
-        bookName: bookName.value,
-        author: author.value,
-        publisher: publisher.value,
-      };
-
-      var bookInfoArray = JSON.parse(localStorage.getItem("bookInfo")) || [];
-      bookInfoArray.push(bookInfo);
-
-      localStorage.setItem("bookInfo", JSON.stringify(bookInfoArray));
-
-      // Show alert
-      alert("BookInfo submitted successfully!");
-
-      // Clear form input fields
-      bookName.value = "";
-      author.value = "";
-      publisher.value = "";
-    });
+    localStorage.setItem("bookInfo", JSON.stringify(bookInfoArray));
+    bookName.value = "";
+    author.value = "";
+    publisher.value = "";
   });
-};
+});
--- UNIT5/src/insert/insert.js
+++ UNIT5/src/insert/insert.js
@@ -1,33 +1,26 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
+  var form = document.querySelector("form");
 
-  // Handle form submission
-  document.querySelector("form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    var form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
+    var bookName = document.getElementById("bookName");
+    var author = document.getElementById("author");
+    var publisher = document.getElementById("publisher");
 
-      var bookName = document.getElementById("bookName");
-      var author = document.getElementById("author");
-      var publisher = document.getElementById("publisher");
+    var bookInfo = {
+      bookName: bookName.value,
+      author: author.value,
+      publisher: publisher.value,
+    };
 
-      var bookInfo = {
-        bookName: bookName.value,
-        author: author.value,
-        publisher: publisher.value,
-      };
+    var bookInfoArray = JSON.parse(localStorage.getItem("bookInfo")) || [];
+    bookInfoArray.push(bookInfo);
+    alert("bookinfo inserted.");
 
-      var bookInfoArray = JSON.parse(localStorage.getItem("bookInfo")) || [];
-      bookInfoArray.push(bookInfo);
-
-      localStorage.setItem("bookInfo", JSON.stringify(bookInfoArray));
-
-      alert("BookInfo submitted successfully!");
-      
-      bookName.value = "";
-      author.value = "";
-      publisher.value = "";
-    });
+    localStorage.setItem("bookInfo", JSON.stringify(bookInfoArray));
+    bookName.value = "";
+    author.value = "";
+    publisher.value = "";
   });
-};
+});
